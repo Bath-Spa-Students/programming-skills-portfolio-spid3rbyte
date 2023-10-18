d35893b2a1a6a7f34b36a630812e750563009bb6
aysa <aysaakther17@gmail.com>
2023-10-18 21:32:14 +0400

    chapter 3 - exercise 3

---
+++ Chapter 3- Data Structures/Exercises/my exercises/exercise 3.py
@@ -0,0 +1,12 @@
+cars = ["ferrari", "lamborghini", "mustang", "bugatti"]
+msg_1 = "I'd like to ride in a " + cars[2]
+print(msg_1)
+
+msg_2 = "I'd love to own a " + cars[0]
+print(msg_2)
+
+msg_3 = "I have hardly seen a " + cars[3] + " in my life"
+print(msg_3)
+
+msg_4 = cars[1] + " looks good"
+print(msg_4)

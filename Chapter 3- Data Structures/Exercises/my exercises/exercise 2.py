d9a577e618bbc95fcec11b48379360f46f6f375d
aysa <aysaakther17@gmail.com>
2023-10-18 21:31:58 +0400

    chapter 3 - exercise 2

---
+++ Chapter 3- Data Structures/Exercises/my exercises/exercise 2.py
@@ -0,0 +1,9 @@
+names = ["Alisha", "Fayha", "Naznin", "Yusra"]
+msg_1 = "Hello " + names[0] + " how are you?"
+print(msg_1)
+msg_2 = "Hello " + names[1] + " how are you?"
+print(msg_2)
+msg_3 = "Hello " + names[2] + " how are you?"
+print(msg_3)
+msg_4 = "Hello " + names[3] + " how are you?"
+print(msg_4)

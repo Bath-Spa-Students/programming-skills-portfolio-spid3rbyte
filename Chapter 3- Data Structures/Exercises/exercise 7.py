bec9be5ab395988e6fd9d91a5cd887a0f7a981f6
aysa <aysaakther17@gmail.com>
2023-10-24 21:12:40 +0400

    chapter 3 - exercise 7

---
+++ Chapter 3- Data Structures/Exercises/exercise 7.py
@@ -0,0 +1,21 @@
+#in original order
+places = ["Palestine", "Turkey", "London", "Dhaka", "Oman"]
+print(places)
+#alphabetical order
+print(sorted(places))
+print(places)
+#reverse alphabetical order 
+print(sorted(places, reverse= True))
+print(places)
+#reversed list 
+places.reverse()
+print(places)
+#reversed original list
+places.reverse()
+print(places)
+#sorted
+places.sort()
+print(places)
+#reverse alphabetical order (again)
+places.sort(reverse=True)
+print(places)

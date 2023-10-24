e67f91c70ad2a771d9a77ac18ed0df4e0b8ef206
aysa <aysaakther17@gmail.com>
2023-10-24 21:12:16 +0400

    chapter 3 - exercise 5

---
+++ Chapter 3- Data Structures/Exercises/my exercises/exercise 5.py
@@ -0,0 +1,18 @@
+dinner_people =  ["Mahmoud Darwish", "Shefale Akther", "Alisha Rizwan"]
+dinner_1 = dinner_people[0]+ " please join my dinner" 
+print(dinner_1)
+dinner_2 = dinner_people[1]+ " please join my dinner" 
+print(dinner_2)
+dinner_3 = dinner_people[2]+ " please join my dinner" 
+print(dinner_3)
+
+print(dinner_people[0] + " can't make it to dinner :(")
+
+dinner_people[0]="Naznin Jamshed"
+
+dinner_1 = dinner_people[0]+ " please join my dinner" 
+print(dinner_1)
+dinner_2 = dinner_people[1]+ " please join my dinner" 
+print(dinner_2)
+dinner_3 = dinner_people[2]+ " please join my dinner" 
+print(dinner_3)

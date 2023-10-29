d79089dd62acdd1bf30dc4b7ea5e142cdb4dcd4a
aysa <aysaakther17@gmail.com>
2023-10-29 21:34:58 +0400

    chapter 4 - exercise 2

---
+++ Chapter 4- Control Flow/Exercises/my exercises/exercise 2.py
@@ -0,0 +1,13 @@
+# runs the if block
+alien_color = "green"
+if alien_color == "green":
+    print("You earned 5 points for shooting the alien")
+else:
+    print ("You earned 10 points for shooting the alien")
+
+# runs the else block 
+alien_color = "red"
+if alien_color == "green":
+    print("You earned 5 points for shooting the alien")
+else:
+    print("You earned 10 points for shooting the alien") 

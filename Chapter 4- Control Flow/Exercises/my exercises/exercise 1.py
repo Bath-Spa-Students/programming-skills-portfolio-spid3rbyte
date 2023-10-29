151f9706c83f4cce5442975f601a3973a01a09ce
aysa <aysaakther17@gmail.com>
2023-10-29 21:34:45 +0400

    chapter 4 - exercise 1

---
+++ Chapter 4- Control Flow/Exercises/my exercises/exercise 1.py
@@ -0,0 +1,8 @@
+#passes the if test
+alien_color = "green"
+if alien_color == "green":
+    print("You have earned 5 points")
+#doesn't pass the if test 
+alien_color = "yellow"
+if alien_color == "green":
+    print("You have earned 5 points")

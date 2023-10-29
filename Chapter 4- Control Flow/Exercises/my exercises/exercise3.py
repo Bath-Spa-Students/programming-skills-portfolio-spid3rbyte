227cf0e5899a702038700fcca79b55e8d25c9f87
aysa <aysaakther17@gmail.com>
2023-10-29 21:40:38 +0400

    Revert "Revert "chapter 3 - exercise 3""
    This reverts commit e4a5cd5da06713903cf3f1f70a055af0ee81466b.

---
+++ Chapter 4- Control Flow/Exercises/my exercises/exercise3.py
@@ -0,0 +1,22 @@
+#green
+alien_color = "green"
+if alien_color == "green":
+    print("You earned 5 points for shooting the alien")
+else:
+    print ("You earned 10 points for shooting the alien")
+
+#yellow
+alien_color = "yellow"
+if alien_color == "yellow":
+    print("You earned 5 points for shooting the alien")
+else:
+    print("You earned 10 points for shooting the alien") 
+
+#red
+alien_color = "red"
+if alien_color == "green":
+    print("You earned 5 points for shooting the alien")
+elif alien_color == "yellow":
+    print("You earned 10 points for shooting the alien") 
+else:
+    print("You earned 15 points for shooting the alien")

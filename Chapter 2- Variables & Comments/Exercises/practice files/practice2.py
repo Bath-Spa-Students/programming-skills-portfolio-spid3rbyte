3f3f681157d716c390a48ea2cd1adb943588e85f
aysa <aysaakther17@gmail.com>
2023-11-02 14:00:28 +0400

    chap 2 - practice file 2

---
+++ Chapter 2- Variables & Comments/Exercises/practice files/practice2.py
@@ -0,0 +1,7 @@
+science = int(input("Enter your marks for science course: "))
+math = int(input("Enter your marks for maths course: "))
+sum = science + math
+average = sum/2
+total_marks = int(input("Enter total marks: "))
+percentage = sum/total_marks*100
+print(percentage)

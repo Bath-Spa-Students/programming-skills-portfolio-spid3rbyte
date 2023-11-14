78d6c55625f1d75661694d29fe6ce1d2b18baa12
aysa <aysaakther17@gmail.com>
2023-11-14 23:32:45 +0400

    chapter 5 - exercise 1

---
+++ Chapter 5- Dictionaries/my exercises/exercise1.py
@@ -0,0 +1,10 @@
+person_info = {
+    'first_name': 'Naznin',
+    'last_name': 'Jamshed',
+    'age': 17,
+    'city': 'Ajman',
+    }
+print(person_info['first_name'])
+print(person_info['last_name'])
+print(person_info['age'])
+print(person_info['city'])

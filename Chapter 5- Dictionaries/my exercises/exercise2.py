6ff038c60c28ffb8d2629e196299f73745ed0c21
aysa <aysaakther17@gmail.com>
2023-11-14 23:32:57 +0400

    chapter 5 - exercise 2

---
+++ Chapter 5- Dictionaries/my exercises/exercise2.py
@@ -0,0 +1,21 @@
+glossary = {
+    'comment':'comments help the user with code',
+    'float': 'float converts numbers to decimal points',
+    'integer': 'integer helps numbers stay as a whole number',
+    'string': "a collection of alphabets and numbers",
+    'print': "command to print a specific message to the screen",
+}
+title_1 = "comment"
+print("\n" + title_1 + ": " + glossary['comment'])
+
+title_2 = "float"
+print("\n" + title_2 + ": " + glossary['float'])
+
+title_3 = "integer"
+print("\n" + title_3 + ": " + glossary['integer'])
+
+title_4 = "string"
+print("\n" + title_4 + ": " + glossary['string'])
+
+title_5 = "print"
+print("\n" + title_5 + ": " + glossary['print'])

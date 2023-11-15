097f73c58d668a0c13f8f72ceb6358465089395f
aysa <aysaakther17@gmail.com>
2023-11-15 22:06:32 +0400

    chapter 7 - exercise 4

---
+++ Chapter 7- Functions/Exercises/exercise4.py
@@ -0,0 +1,7 @@
+def make_shirt(size = 'large', text = 'i love python'):
+    print("the shirt will be of " + size + " size")
+    print("it will have " + text + " written on it")
+
+make_shirt('large','"i love python"')
+make_shirt('medium', '"i love python"')
+make_shirt('small', '"i want to sleep"')

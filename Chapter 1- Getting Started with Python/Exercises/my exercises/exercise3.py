e55c045555964516106ff96e425e61a77298482b
aysa <aysaakther17@gmail.com>
2023-11-15 22:07:16 +0400

    chapter 1 - exercise 3

--- Chapter 1- Getting Started with Python/Exercises/my exercises/exercise3.py
+++ Chapter 1- Getting Started with Python/Exercises/my exercises/exercise3.py
@@ -1,7 +1,8 @@
-
 from datetime import datetime
 
 now = datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
+current_date = now.strftime("%d/%m/%Y")
 print("Current Time =", current_time)
+print("Current Date =", current_date)

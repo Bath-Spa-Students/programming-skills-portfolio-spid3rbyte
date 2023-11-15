fcf3fdc45dc9e2f971fca160da30e9cfa3372dee
aysa <aysaakther17@gmail.com>
2023-11-15 22:06:40 +0400

    chapter 7 - exercise 5

---
+++ Chapter 7- Functions/Exercises/exercise5.py
@@ -0,0 +1,6 @@
+def describe_city(city, country = 'iraq'):
+    print(city + " is in " + country)
+
+describe_city('paris', 'france')
+describe_city('dhaka', 'bangladesh')
+describe_city('abudhabi', 'uae')

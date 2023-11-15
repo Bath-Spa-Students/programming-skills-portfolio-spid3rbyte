42423ddc1a934259b730b7934b17e4a82cb05dd8
aysa <aysaakther17@gmail.com>
2023-11-15 22:06:21 +0400

    chapter 7 - exercise 3

---
+++ Chapter 7- Functions/Exercises/exercise3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, text):
+    print("the shirt will be of " + size + " size")
+    print("it will have " + text + " written on it")
+
+make_shirt('small','"python makes me want to cry"')
+make_shirt('XL', '"i love staying at home"')
